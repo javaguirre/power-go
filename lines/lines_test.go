60e30eed644b6df83084074cddcdb1356f072a38
Javier Aguirre <contacto@javaguirre.net>
2022-09-26 11:05:34 +0200

    Updated testing

--- lines/lines_test.go
+++ lines/lines_test.go
@@ -2,16 +2,18 @@ package lines_test
 
 import (
 	"bytes"
+	"os"
 	"testing"
 
 	"github.com/power-go/lines"
 )
 
-func TestLines(t *testing.T) {
+func TestLinesValidInputOutput(t *testing.T) {
 	t.Parallel()
 	inputBuf := bytes.NewBufferString("1\n2\n3\n")
 	c, err := lines.NewCounter(
 		lines.WithInput(inputBuf),
+		lines.WithOutput(os.Stdout),
 	)
 	want := 3
 
@@ -23,5 +25,30 @@ func TestLines(t *testing.T) {
 
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestLinesInvalidInputOutput(t *testing.T) {
+	t.Parallel()
+	_, err := lines.NewCounter(
+		lines.WithInput(nil),
+		lines.WithOutput(nil),
+	)
+
+	if err == nil {
+		t.Errorf("Wanted error")
+	}
+}
+
+func TestLinesValidInputInvalidOutput(t *testing.T) {
+	t.Parallel()
+	inputBuf := bytes.NewBufferString("1\n2\n3\n")
+	_, err := lines.NewCounter(
+		lines.WithInput(inputBuf),
+		lines.WithOutput(nil),
+	)
+
+	if err == nil {
+		t.Errorf("Wanted error")
 	}
 }
